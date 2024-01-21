8c32b157b2c94c9bf108ce15c747d0239204bf82
ArnavRupde <arnavrupade@gmail.com>
2024-01-21 15:31:13 +0530

    Add health check endpoint

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Status: OK")
+}
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Starting HTTP Server on port 8000")
+	http.HandleFunc("/health", healthCheckHandler)
+	http.ListenAndServe(":8000", nil)
 }
